1ba107bc9b5cf6f998fc14ee59239f850aabbf80
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-08-20 22:39:38 -0700

    feat: add validators to settings

--- src/fishsense_api_workflow_worker/config.py
+++ src/fishsense_api_workflow_worker/config.py
@@ -1,10 +1,40 @@
 """Dynaconf settings module."""
 
-from dynaconf import Dynaconf
+import os
+from pathlib import Path
+
+import validators
+from dynaconf import Dynaconf, Validator
+
+IS_DOCKER = os.environ.get("E4EFS_DOCKER", False)
+
+
+def get_config_path() -> Path:
+    """Get config path
+
+    Returns:
+        Path: Path to config directory
+    """
+    if IS_DOCKER:
+        return Path("/e4efs/config")
+    config_path = Path(".")
+    return config_path
+
+
+validators = [
+    Validator("temporal.host", required=True, cast=str),
+    Validator("temporal.port", required=True, cast=str, default=7233),
+]
 
 settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=["settings.toml", ".secrets.toml"],
+    envvar_prefix="E4EFS",
+    environments=False,
+    settings_files=[
+        (get_config_path() / "settings.toml").as_posix(),
+        (get_config_path() / ".secrets.toml").as_posix(),
+    ],
+    merge_enabled=True,
+    validators=validators,
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
