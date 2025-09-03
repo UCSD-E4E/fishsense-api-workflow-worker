275fd59b4fb70bbc0eb1783d68308828f19b7c60
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-09-03 02:28:12 -0700

    fix: pydantic allow null user ids

--- src/fishsense_api_workflow_worker/models/user.py
+++ src/fishsense_api_workflow_worker/models/user.py
@@ -8,12 +8,12 @@ from sqlmodel import Field, SQLModel
 class User(SQLModel, table=True):
     """Model representing a user."""
 
-    id: int = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     email: str = Field(max_length=100, unique=True)
     first_name: str = Field(max_length=100)
     last_name: str = Field(max_length=100)
-    last_activity: datetime = Field(default=None)
-    date_joined: datetime = Field(default=None)
+    last_activity: datetime | None = Field(default=None)
+    date_joined: datetime | None = Field(default=None)
 
     @classmethod
     def from_label_studio(cls, user) -> "User":
